603f4eeea13535b5cd63d395ac51e4e39f2c2949
Sagor8187 <sdsagor8187@gmail.com>
2026-05-06 15:11:04 +0600

    tow problem solve

---
+++ duplicateRemove.js
@@ -0,0 +1,21 @@
+//let nums [1,2,2,3,4,4,5] 
+// output→ [1,2,3,4,5]
+
+let nums = [1,2,2,3,4,4,5] 
+const filters = []
+
+for (let i = 0;i<nums.length;i++){
+    let isduplicate = false;
+
+    for(let j = 0;j<nums.length; j++){
+        if(nums[i]=== filters[j]){
+            isduplicate =true
+            break
+        }
+    }
+    if(!isduplicate){
+        filters.push(nums[i])
+    }
+}
+
+console.log(filters)
---
+++ targetSum.js
@@ -0,0 +1,16 @@
+// Task :
+// nums = [2,7,11,15], target = 9
+// output: [0,1]
+
+let nums = [2,7,12,11,15]
+
+let target = 9
+
+for(let i=0;i<nums.length;i++ ){
+  for(let j = i+1 ; j<nums.length;j++){
+        if(nums[i] + nums[j]=== target){
+            console.log([i,j])
+        }
+  }
+}
+
